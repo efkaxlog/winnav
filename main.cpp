4936710abb0d709b1fb853c21d5aebcffea99e7e
Filip Kowalczyk <efkaxlog@gmail.com>
2017-11-02 11:00:30 +0000

    Add arbitrary shortcuts to test sending windows to the corners. Working.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,10 @@ int main(int argc, char** argv) {
     RegisterHotKey(NULL, BOTTOM, MOD_SHIFT | MOD_ALT, VK_DOWN);
     RegisterHotKey(NULL, LEFT, MOD_SHIFT | MOD_ALT, VK_LEFT);
     RegisterHotKey(NULL, RIGHT, MOD_SHIFT | MOD_ALT, VK_RIGHT);
+    RegisterHotKey(NULL, TOP_LEFT, MOD_SHIFT | MOD_ALT, 0x49); // I
+    RegisterHotKey(NULL, BOT_LEFT, MOD_SHIFT | MOD_ALT, 0x4B); // K
+    RegisterHotKey(NULL, TOP_RIGHT, MOD_SHIFT | MOD_ALT, 0x4F); // O
+    RegisterHotKey(NULL, BOT_RIGHT, MOD_SHIFT | MOD_ALT, 0x4C); // L
     RegisterHotKey(NULL, QUIT, MOD_SHIFT | MOD_ALT, 0x51); // Q key
     MSG msg;
     while(GetMessage(&msg, NULL, 0, 0)) {
