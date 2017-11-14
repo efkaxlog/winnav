3d29b801ef7574630b6a324c5bbe4c87836cd96f
Kowalczyk Filip <funkoverflow@gmail.com>
2017-11-14 14:03:46 +0000

    Add moving windows on multiple monitors feature and add some debugging prints.

--- main.cpp
+++ main.cpp
@@ -9,11 +9,19 @@
  * 
  */
 int main(int argc, char** argv) {
-    RegisterHotKey(NULL, TOP, MOD_SHIFT | MOD_ALT, VK_UP);
-    RegisterHotKey(NULL, BOTTOM, MOD_SHIFT | MOD_ALT, VK_DOWN);
-    RegisterHotKey(NULL, LEFT, MOD_SHIFT | MOD_ALT, VK_LEFT);
-    RegisterHotKey(NULL, RIGHT, MOD_SHIFT | MOD_ALT, VK_RIGHT);
+	// For now or maybe permanently, default keys for moving windows will be
+	// J, I, K, L for top, bottom, left, right, and
+	// N, M, comma, full stop, for corners.
+    RegisterHotKey(NULL, TOP, MOD_SHIFT | MOD_ALT, 0x49); // I key
+    RegisterHotKey(NULL, BOTTOM, MOD_SHIFT | MOD_ALT, 0x4B); // K key
+    RegisterHotKey(NULL, LEFT, MOD_SHIFT | MOD_ALT, 0x4A); // J key
+	RegisterHotKey(NULL, RIGHT, MOD_SHIFT | MOD_ALT, 0x4C); // L key
+	RegisterHotKey(NULL, TOP_LEFT, MOD_SHIFT | MOD_ALT, 0x4E); // N key
+	RegisterHotKey(NULL, TOP_RIGHT, MOD_SHIFT | MOD_ALT, 0x4D); // M key
+	RegisterHotKey(NULL, BOT_LEFT, MOD_SHIFT | MOD_ALT, 0xBC); // comma key
+	RegisterHotKey(NULL, BOT_RIGHT, MOD_SHIFT | MOD_ALT, 0xBE); // full stop key
     RegisterHotKey(NULL, QUIT, MOD_SHIFT | MOD_ALT, 0x51); // Q key
+
     MSG msg;
     while(GetMessage(&msg, NULL, 0, 0)) {
         char message = msg.wParam;
--- winnav.cpp
+++ winnav.cpp
@@ -23,42 +23,55 @@ void winnav::moveWindow(char loc) {
     HWND handle = GetForegroundWindow();
     HMONITOR monitorHandle = MonitorFromWindow(handle, MONITOR_DEFAULTTONEAREST);
     RECT monitorRect = winnav::getMonitorWorkRect(monitorHandle);
+	std::cout << "============\nMonitor Work Rect: \n";
+	std::cout << "x: " << monitorRect.left << std::endl;
+	std::cout << "y: " << monitorRect.top << std::endl;
+	std::cout << "right: " << monitorRect.right << std::endl;
+	std::cout << "bottom: " << monitorRect.bottom << std::endl;
+
     // new window coordinates
     int x = monitorRect.left;
     int y = monitorRect.top;
-    int width, height; 
-    int halfRight = monitorRect.right / 2;
-    int halfBottom = monitorRect.bottom / 2;
-    
-    if (loc == TOP || loc == BOTTOM) {
-            x = monitorRect.left;
-            height = halfBottom;
-            width = monitorRect.right;
-    } else if (loc == LEFT || loc == RIGHT) {
-        y = monitorRect.top;
-        height = monitorRect.bottom;
-        width = halfRight;
-    } else {
-        height = halfBottom;
-        width = halfRight;
-    }
-    
-    switch(loc) {
-        case BOTTOM:
-        case BOT_LEFT:
-            y = halfBottom;
-            break;
-        case RIGHT:
-        case TOP_RIGHT:
-            x = halfRight;
-            break;
-        case BOT_RIGHT:
-            x = halfRight;
-            y = halfBottom;
-            break;
-        default:
-            break;
-    }
+	int width = monitorRect.right - x;
+	int height = monitorRect.bottom;
+    int halfRight = width / 2;
+    int halfBottom = height / 2;
+
+	switch (loc) {
+	case TOP:
+		height = halfBottom;
+		break;
+	case BOTTOM:
+		y = halfBottom;
+		height = halfBottom;
+		break;
+	case LEFT:
+		width = halfRight;
+		break;
+	case RIGHT:
+		x += halfRight;
+		width = halfRight;
+		break;
+	default:
+		width = halfRight;
+		height = halfBottom;
+		if (loc == TOP_RIGHT) {
+			x += halfRight;
+		} else if (loc == BOT_LEFT) {
+			y = halfBottom;
+		}
+		else if (loc == BOT_RIGHT) {
+			x += halfRight;
+			y = halfBottom;
+		}
+		break;
+	}
+
+	std::cout << "--------------\nMoving to: \n";
+	std::cout << "x: " << x << std::endl;
+	std::cout << "y: " << y << std::endl;
+	std::cout << "width: " << width << std::endl;
+	std::cout << "height: " << height << std::endl;
     SetWindowPos(handle, NULL, x, y, width, height, SWP_SHOWWINDOW);
 }
 
